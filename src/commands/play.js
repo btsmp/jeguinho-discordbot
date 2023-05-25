8fbf21201130de546db3fd1f06564f8841744040
Bruno Sampaio <brunoctsb@gmail.com>
2023-05-25 20:16:10 -0300

    inital commit

---
+++ src/commands/play.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+  .setName('play')
+  .setDescription('Fale uma musica para dar ouvir')
+  .addStringOption(option =>
+    option
+      .setName('nome')
+      .setDescription('Nome da musica')
+      .setRequired(true))
+
+module.exports = {
+  data,
+  async execute(interaction) {
+    const target = interaction.options.getString('nome');
+    await interaction.reply(`essa é a musica ${target}`)
+  }
+}
